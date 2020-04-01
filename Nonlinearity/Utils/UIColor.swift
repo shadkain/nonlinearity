25737c6a280d01c49d96bb8a476af4c1ff8581e9
shadkain <kryzev@gmail.com>
2020-04-01 21:59:48 +0300

    added UIColor hex extension

---
+++ Nonlinearity/Utils/UIColor.swift
@@ -0,0 +1,28 @@
+//
+//  UIColor.swift
+//  Nonlinearity
+//
+//  Created by Святослав Кряжев on 01.04.2020.
+//  Copyright © 2020 Святослав Кряжев. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static func rgb(red: Int, green: Int, blue: Int) -> UIColor {
+        return UIColor(
+            red: CGFloat(red) / 255.0,
+            green: CGFloat(green) / 255.0,
+            blue: CGFloat(blue) / 255.0,
+            alpha: 1.0
+        )
+    }
+    
+    static func hex(rgb: Int) -> UIColor {
+        return .rgb(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
+    }
+}
