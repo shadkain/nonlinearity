6e8203304a1c0df44f810c4b4169b3fd33bdce10
shadkain <kryzev@gmail.com>
2020-04-23 23:18:20 +0300

    header & bottom view

--- Nonlinearity/Pages/Game/BottomView.swift
+++ Nonlinearity/Pages/Game/BottomView.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2020 Святослав Кряжев. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class BottomView: UIViewComponent {
+    private let nextButtonView = UIImageView()
+    
+    override func setup() {
+        nextButtonView.image = .init(imageLiteralResourceName: "next-arrow")
+        
+        [nextButtonView].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            addSubview($0)
+        }
+    }
+    
+    override func constraint() {
+        NSLayoutConstraint.activate([
+            nextButtonView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            nextButtonView.centerXAnchor.constraint(equalTo: centerXAnchor),
+        ])
+    }
+}
